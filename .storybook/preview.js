40a91b0d242677b976c884093d387318264cf978
Ilana Hakim <ilana.hakim@gmail.com>
2023-02-07 17:45:13 +0200

    replace font

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,10 @@
 import { withMuiTheme } from './with-mui-theme.decorator';
 
 // Load Roboto fonts
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import '@fontsource/assistant/300.css';
+import '@fontsource/assistant/400.css';
+import '@fontsource/assistant/500.css';
+import '@fontsource/assistant/700.css';
 import '@fontsource/material-icons';
 
 export const parameters = {
